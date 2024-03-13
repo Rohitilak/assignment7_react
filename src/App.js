8db213fcce242355552539fcc56bab84dc9a903b
Rohitilak <rohittilak086@gmail.com>
2024-03-13 19:40:36 +0530

    add

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Home from './components/Home.js';
+import Navbar from './components/Navbar.js';
+
+function App() {
+  return (
+    <div className="App">
+        <Navbar/>
+        <Home/>
+    </div>
+  );
+}
+
+export default App;
